5a1d8929390a0a72e3b18b21d9e6380a4531baa8
Devcb13 <95518863+Devcb13@users.noreply.github.com>
2022-03-07 08:38:26 +0530

    final

--- Cycle 1/test.cpp
+++ Cycle 1/test.cpp
@@ -1,74 +1,18 @@
-#include<iostream>
-using namespace std ;
+#include <iostream>
+using namespace std;
 
-class matrix 
-{
-    int m,n ;
-    int **p ;
-    public:
-        matrix(){};
-        matrix(int M,int N);
-        friend matrix rr(matrix K);
-        void show();
-};
+int m[3][3] = {{1,2,3},{3,4,5},{5,6,7}};
 
-matrix::matrix(int M , int N)
+
+int det(int a1,int a2,int b1,int b2)
 {
-    m = M ; n = N ;
-    p = new int*[M+1] ;
-    for(int i = 0 ; i < n ; i++){
-        *p = new int[N+1] ;
-    }
-    for(int i = 0; i < m ; i++){
-        cout << "Row " << i + 1 << "\n" ;
-        for(int j = 0 ; j < m ; j++){
-            cout << "elem " << j + 1 << " : ";
-            int elem ;
-            cin >> elem ;
-            p[i][j] = elem ;
-        }
-    }
+    int detm = 0 ;
+    detm = (a1*b2) - (a2*b2);
 }
 
-matrix rr(matrix A)
+int Ddet(int sz)
 {
-    matrix R ;
-    R.m = A.m; R.m = R.n ;
-    //section to copy
-    for(int i = 0 ; i < R.m ; i++){
-        for(int j = 0 ; j < R.n ; j++){
-            R.p[i][j] = A.p[i][j];
-        }
-    }
-    //Row reduction
-    for(int i = 0;i<R.m;i++){
-        for(int j = i;j<R.n;j++){
-            float f = (A.p[j][i])/(A.p[i][i]);
-            for(int k = 0 ; k < A.n ; k++){
-                R.p[j][k] = R.p[j][k] - (f*(R.p[i][k]));
-            }
-        }
+    for(int i = 0 ; i < sz ; i++){
+        
     }
 }
-
-void matrix::show()
-{
-    for(int i = 0 ;i < m ;i++){
-        for(int j = 0 ; j < n ; j++){
-            cout << p[i][j] << " " ;
-        }
-        cout << "\n";
-    }
-    cout << "\n\n";
-}
-
-int main()
-{
-    matrix A(3,3);
-    A.show();
-    matrix B ;
-    B = rr(A);
-    B.show();
-    return 0 ;
-}
-
